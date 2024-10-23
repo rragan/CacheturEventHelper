4d7f87d99dbeb58c2f836681862a4c4087e93b97
Richard Ragan <rragan@paypal.com>
2024-10-23 10:49:08 -0700

    Create CacheturEventHelper.meta.js
    Meta file

---
+++ CacheturEventHelper.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name            Cachetur event time helper
+// @author          cachetur.no, thomfre, rragan (derived from cachetur assistant)
+// @namespace       http://cachetur.no/
+// @version         1.0.0
+// @description     Script to fill in event start/end times for cachetur
+// @match           https://cachetur.no/admin/eventer
+// @connect         cachetur.no
+// @connect         cachetur.net
+// @connect         geocaching.com
+// @connect         self
+// @grant           GM_xmlhttpRequest
+// @grant           unsafeWindow
+// @run-at          document-end
+// @copyright       2024+, rragan
+// @require         https://raw.githubusercontent.com/cghove/GM_config/master/gm_config.js
+// @require         https://code.jquery.com/jquery-latest.js
+// @require         https://unpkg.com/i18next@21.9.1/i18next.min.js
+// @require         https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @downloadURL     https://github.com/rragan/CacheturEventHelper/raw/main/CacheturEventHelper.user.js
+// ==/UserScript==
